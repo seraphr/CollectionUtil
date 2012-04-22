761b1ffd9dcfa89b4cc54f7f30a634b0a44993a5
seraph <local@seraphr.jp>
2012-04-23 04:47:04 +0900

    first commit

---
+++ src/main/java/jp/seraphr/collection/App.java
@@ -0,0 +1,13 @@
+package jp.seraphr.collection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
