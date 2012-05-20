91f65c0baf3769ea08121522241c2cebd378a3d7
seraph <local@seraphr.jp>
2012-05-20 21:04:41 +0900

    CollectionWrapperのパッケージを変更

--- src/main/java/jp/seraphr/collection/CollectionWrapper.java
+++
@@ -1,7 +0,0 @@
-package jp.seraphr.collection;
-
-public interface CollectionWrapper<_Base, _Element> {
-    public <_ToElem, _To> CollectionWrapper<_To, _ToElem> map(Converter<_Element, _ToElem> aConverter);
-
-    public _Base unwrap();
-}
---
+++ src/main/java/jp/seraphr/collection/wrapper/CollectionWrapper.java
@@ -0,0 +1,22 @@
+package jp.seraphr.collection.wrapper;
+
+import jp.seraphr.collection.CollectionUtils;
+import jp.seraphr.collection.Converter;
+import jp.seraphr.collection.builder.WrapperBuilder;
+
+public abstract class CollectionWrapper<_Base extends Iterable<_Element>, _Element> {
+
+    private _Base mBase;
+
+    protected <_ToBase extends Iterable<_ToElem>, _ToElem, _To extends CollectionWrapper<_ToBase, _ToElem>> _To mapInner(Converter<_Element, _ToElem> aConverter, _ToBase aDummy){
+        WrapperBuilder<_ToBase, _ToElem, _To> tBuilder = builder(aDummy);
+
+        return CollectionUtils.map(mBase, tBuilder, aConverter);
+    }
+
+    protected abstract <_ToBase extends Iterable<_ToElem>, _ToElem, _To extends CollectionWrapper<_ToBase, _ToElem>> WrapperBuilder<_ToBase, _ToElem, _To> builder(_ToBase aDummy);
+
+    public _Base unwrap() {
+        return mBase;
+    }
+}
