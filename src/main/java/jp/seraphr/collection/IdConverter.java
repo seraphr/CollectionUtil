32f7d47604a35e758313cdfe257081c6959501a7
seraph <local@seraphr.jp>
2012-06-04 00:14:01 +0900

    IdConverter追加

---
+++ src/main/java/jp/seraphr/collection/IdConverter.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+package jp.seraphr.collection;
+
+/**
+ * 変換を行わないConverter実装
+ */
+public class IdConverter<_T> implements Converter<_T, _T> {
+    @Override
+    public _T convert(_T aSource) {
+        return aSource;
+    }
+}
