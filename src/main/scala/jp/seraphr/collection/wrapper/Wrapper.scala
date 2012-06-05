f1303c79992a9132fe66c88611a32201746016a8
seraph <local@seraphr.jp>
2012-06-05 11:15:14 +0900

    Wrapperのコメントアウトを解除

--- src/main/scala/jp/seraphr/collection/wrapper/Wrapper.scala
+++ src/main/scala/jp/seraphr/collection/wrapper/Wrapper.scala
@@ -10,17 +10,16 @@ trait Wrapper[_Elem] {
     type _This <: Wrapper[_Elem]
 
     protected def mapInner[_ToElem, _To <: Wrapper[_ToElem]](aConverter: Converter[_Elem, _ToElem])(aBuilder: WrapperBuilder[_ToElem, _To]): _To = {
-//        CollectionUtils.map(toIterable, aBuilder, aConverter)
-      throw new RuntimeException
+        CollectionUtils.map(toIterable, aBuilder, aConverter)
     }
 
-//    def filter(aPredicate: Predicate[_Elem]): _This = {
-//      CollectionUtils.filter(toIterable, myBuilder, aPredicate)
-//    }
-//
-//    def filterNot(aPredicate: Predicate[_Elem]): _This = {
-//      CollectionUtils.filterNot(toIterable, myBuilder, aPredicate)
-//    }
+    def filter(aPredicate: Predicate[_Elem]): _This = {
+      CollectionUtils.filter(toIterable, myBuilder, aPredicate)
+    }
+
+    def filterNot(aPredicate: Predicate[_Elem]): _This = {
+      CollectionUtils.filterNot(toIterable, myBuilder, aPredicate)
+    }
 
 //    def foldLeft[_Result](_Result aInitValue, Con)
 
