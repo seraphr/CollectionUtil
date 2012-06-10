d019a3771688171b0cb5231dee04d568b62c9d56
seraph <local@seraphr.jp>
2012-06-10 10:57:20 +0900

    Wrapper#toIterableがnullになってしまうのを修正

--- src/main/scala/jp/seraphr/collection/wrapper/ListWrapper.scala
+++ src/main/scala/jp/seraphr/collection/wrapper/ListWrapper.scala
@@ -1,7 +1,7 @@
 package jp.seraphr.collection.wrapper
 
 import java.util.List
-  import java.lang.Iterable
+import java.lang.Iterable
 import jp.seraphr.collection.builder.ListWrapperBuilder
 import jp.seraphr.common.Converter
 import jp.seraphr.common.Tuple2
@@ -11,12 +11,11 @@ class ListWrapper[_Elem](aBase: List[_Elem]) extends Wrapper[_Elem] {
   type _Base = List[_Elem]
   type _This = ListWrapper[_Elem]
 
-
   def map[_ToElem](aConverter: Converter[_Elem, _ToElem]): ListWrapper[_ToElem] = {
     mapInner(aConverter)(new ListWrapperBuilder[_ToElem])
   }
 
-  def zip[_ThatElem](aThat: Iterable[_ThatElem]): ListWrapper[Tuple2[_Elem, _ThatElem]] ={
+  def zip[_ThatElem](aThat: Iterable[_ThatElem]): ListWrapper[Tuple2[_Elem, _ThatElem]] = {
     zipInner(aThat)(new ListWrapperBuilder[Tuple2[_Elem, _ThatElem]])
   }
 
@@ -24,6 +23,9 @@ class ListWrapper[_Elem](aBase: List[_Elem]) extends Wrapper[_Elem] {
 
   override val unwrap = aBase
 
-  override protected def toIterable(aBase: _Base): _Container[_Elem] = unwrap
+  override protected def toIterable(aBase: _Base): _Container[_Elem] = {
+//    require(aBase != null)
+    unwrap
+  }
 
 }
