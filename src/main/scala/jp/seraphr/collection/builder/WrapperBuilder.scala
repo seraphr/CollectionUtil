2278892b6221dc5d681ba90086cbe37efcd84ccf
seraph <local@seraphr.jp>
2012-07-05 01:53:41 +0900

    wrapper部分を外部に分離

--- src/main/scala/jp/seraphr/collection/builder/WrapperBuilder.scala
+++
@@ -1,6 +0,0 @@
-package jp.seraphr.collection.builder
-import jp.seraphr.collection.wrapper.Wrapper
-
-trait WrapperBuilder[_Elem, _Wrapper <: Wrapper[_Elem, _Wrapper]] extends Builder[_Elem, _Wrapper] {
-
-}
